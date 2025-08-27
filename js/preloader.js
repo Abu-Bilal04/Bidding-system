361aab0cc294675a813fbfabe5a7eff04eb25f2b
Hello 'Dansa Ido <151717971+Abu-Bilal04@users.noreply.github.com>
2025-08-28 00:00:56 +0100

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,5 @@
+// Preloader functionality
+      window.addEventListener("load", function () {
+        document.getElementById("preloader").style.display = "none";
+        document.getElementById("mainContent").style.display = "block";
+      });
